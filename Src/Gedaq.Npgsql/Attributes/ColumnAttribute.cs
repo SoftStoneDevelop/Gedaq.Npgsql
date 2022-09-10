0625d099121292663b181571dd947dd761c82c8d
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2022-09-10 13:41:12 +0300

    feat: init

---
+++ Src/Gedaq.Npgsql/Attributes/ColumnAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Gedaq.Npgsql.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public sealed class ColumnAttribute : Gedaq.Provider.Attributes.ColumnAttribute
+    {
+        public ColumnAttribute(
+            string nameInDatabase,
+            NpgsqlTypes.NpgsqlDbType type
+            )
+            : base(nameInDatabase)
+        {
+        }
+    }
+}
---
+++ Src/Gedaq.Npgsql/Attributes/NpgsqlProvider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Gedaq.Npgsql.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public abstract class NpgsqlProvider : Gedaq.Provider.Attributes.ProviderAttribute
+    {
+    }
+}
