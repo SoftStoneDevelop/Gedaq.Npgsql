d06ace7ba7597c8e148b05bb2e331523e47a87e6
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2022-09-10 14:22:23 +0300

    feat: default text attribute

---
+++ Src/Gedaq.Npgsql/Attributes/Defaults/DefaultText.cs
@@ -0,0 +1,14 @@
+﻿namespace Gedaq.Npgsql.Attributes.Defaults
+{
+    public sealed class DefaultTextAttribute : Gedaq.Provider.Attributes.Constraints.DefaultAttribute
+    {
+        public DefaultTextAttribute(
+            string constrainName,
+            string defaultValue
+            )
+            : base(constrainName)
+        {
+
+        }
+    }
+}
