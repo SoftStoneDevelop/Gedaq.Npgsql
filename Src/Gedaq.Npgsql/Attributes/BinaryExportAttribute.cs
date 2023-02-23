e0833274f7aee5fcffc8a90bf0787da2242f974c
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2023-02-23 04:48:18 +0300

    feat: BinaryExportAttribute

---
+++ Src/Gedaq.Npgsql/Attributes/BinaryExportAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Gedaq.Common.Enums;
+using Gedaq.Npgsql.Enums;
+using System;
+
+namespace Gedaq.Npgsql.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method, AllowMultiple = true)]
+    public sealed class BinaryExportAttribute : Attribute
+    {
+        public BinaryExportAttribute(
+            string query,
+            string methodName,
+            Type queryMapType,
+            MethodType methodType = MethodType.Sync,
+            SourceType sourceType = SourceType.Connection
+            )
+        {
+        }
+    }
+}
