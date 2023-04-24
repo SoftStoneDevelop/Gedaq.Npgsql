3532d6ca43fc9e9ffface2100bcb95fd5452e865
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2023-04-24 19:49:11 +0300

    feat: resolve #2

---
+++ Src/Gedaq.Npgsql/QueryFormatAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Gedaq.Npgsql
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method, AllowMultiple = true)]
+    public sealed class QueryFormatAttribute : Attribute
+    {
+        public QueryFormatAttribute(
+            string methodName,
+            int position = 0,
+            string parametrName = null
+            )
+        {
+        }
+    }
+}
