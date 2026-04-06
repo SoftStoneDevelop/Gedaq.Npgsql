119ecfecb070d4a4c4448ae4f3ff7a1fa1fb3336
Vyacheslav Brevnov <softstonedevelop@gmail.com>
2026-04-06 23:46:04 +0300

    feat: + Attribute for dynamic query mapping
    #https://github.com/SoftStoneDevelop/Gedaq/issues/58

--- Src/Gedaq.Npgsql/Attributes/DynamicParametrAttribute.cs
+++ Src/Gedaq.Npgsql/Attributes/DynamicParametrAttribute.cs
@@ -5,7 +5,7 @@ namespace Gedaq.Npgsql.Attributes
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method, AllowMultiple = true)]
     public sealed class DynamicParametrAttribute : Attribute
     {
-        public DynamicParametrAttribute(bool isPositionParametrs = false)
+        public DynamicParametrAttribute()
         {
         }
     }
